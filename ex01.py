5e78eebd632222e5253a6629a2ef1c7e0f7f0737
pavol <devnull@localhost>
2013-03-27 12:07:33 -0400

    added the second line

--- ex01.py
+++ ex01.py
@@ -1,7 +1,15 @@
 # coding: utf-8
 cds=loadStructure('CdS_wurtzite.cif')
 pc1=PDFCalculator()
+pc1.rmax=20
 pc1.scatteringfactortable.setCustomAs('S2-', 'S', 18)
 pc1.scatteringfactortable.lookup('S2-')
 r1,g1=pc1(cds)
 plot(r1,g1)
+
+pc2 = pc1.copy()
+cds2=loadStructure('CdS_wurtzite.cif')
+cds2.anisotropy = False
+r2,g2=pc2(cds2)
+plot(r2,g2)
+plot(r1, g1-g2)
