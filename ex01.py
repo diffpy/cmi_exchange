b7c370ce1967ef2a0c844a207a68d774c3c0c7ec
Pavol Juhas <pavol.juhas@gmail.com>
2013-07-23 19:11:19 -0400

    Add imports to make it a legal script.
    Also fix punctuation.

--- ex01.py
+++ ex01.py
@@ -1,15 +1,22 @@
-# coding: utf-8
-cds=loadStructure('CdS_wurtzite.cif')
-pc1=PDFCalculator()
-pc1.rmax=20
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from diffpy.Structure import loadStructure
+from diffpy.srreal.pdfcalculator import PDFCalculator
+from matplotlib.pyplot import plot, show
+
+cds = loadStructure('CdS_wurtzite.cif')
+pc1 = PDFCalculator()
+pc1.rmax = 20
 pc1.scatteringfactortable.setCustomAs('S2-', 'S', 18)
 pc1.scatteringfactortable.lookup('S2-')
-r1,g1=pc1(cds)
-plot(r1,g1)
+r1, g1 = pc1(cds)
+plot(r1, g1)
 
 pc2 = pc1.copy()
-cds2=loadStructure('CdS_wurtzite.cif')
+cds2 = loadStructure('CdS_wurtzite.cif')
 cds2.anisotropy = False
-r2,g2=pc2(cds2)
-plot(r2,g2)
+r2, g2 = pc2(cds2)
+plot(r2, g2)
 plot(r1, g1-g2)
+show()
--- ex02.py
+++ ex02.py
@@ -1,7 +1,16 @@
-# coding: utf-8
-c60=loadStructure('c60.stru')
-dpc=DebyePDFCalculator()
-dpc.qmax=20
-dpc.rmax=20
-r3,g3=dpc(c60, qmin=0)
-r4,g4=dpc(c60, qmin=1)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from diffpy.Structure import loadStructure
+from diffpy.srreal.pdfcalculator import DebyePDFCalculator
+from matplotlib.pyplot import plot, show
+
+c60 = loadStructure('c60.stru')
+dpc = DebyePDFCalculator()
+dpc.qmax = 20
+dpc.rmax = 20
+r3, g3 = dpc(c60, qmin=0)
+r4, g4 = dpc(c60, qmin=1)
+
+plot(r3, g3, r4, g4)
+show()
