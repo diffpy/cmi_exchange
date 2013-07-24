909e0be6ad6ab2fc1293dfccd7d7fd8ef981c350
Pavol Juhas <pavol.juhas@gmail.com>
2013-07-24 19:51:00 -0400

    Edit and convert to Python demo.
    Not finished yet, needs more work from the FIXME marker.

--- ex03.py
+++ ex03.py
@@ -1,83 +1,126 @@
-# coding: utf-8
-figure()
-close()
-xobs=arange(-10, 10.1)
-xobs
-get_ipython().magic(u'pinfo randn')
-yobs=0.5*xobs + 3 + 0.3 * randn(21)
-plot(xobs, yobs, 'x')
-from diffpy.srfit.fitbase import Profile
-help(Profile)
-get_ipython().magic(u'hist')
-linedata=Profile()
-get_ipython().magic(u'pinfo linedata.setObservedProfile')
-linedata.setObservedProfile(xobs, yobs)
-from diffpy.srfit.fitbase import FitContribution
-help(FitContribution)
-linefit=FitContribution('linefit')
-linefit.setProfile(linedata)
-help(linefit.setEquation)
-linefit.show()
-linefit.setEquation("A * x + B")
-linefit.show()
-linefit.A
-linefit.A=3
-linefit.B=5
-linefit.evaluate()
-linefit.show()
-linefit.y
-linefit.profile
-linefit.x.value
-linefit.x
-linefit.x.value
-linefit.profile
-linedata
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from matplotlib.pyplot import plot, show, clf
+
+# Simulate linear data with some random Gaussian noise.
+xobs = np.arange(-10, 10.1)
+yobs = 0.5 * xobs + 3 + 0.3 * np.random.randn(xobs.size)
 clf()
-plot(linedata.x, linedata.yobs)
-plot(linedata.x, linedata.ycalc)
-linedata.ycalc)
-linedata.ycalc
-linedata.y
-linedata.yobs
-plot(xobs, linefit.evaluate())
-get_ipython().magic(u'pinfo linefit.evaluate')
+plot(xobs, yobs, 'x')
+show()
+
+# <demo> --- stop ---
+
+# We are going to define a line fitting regression using srfit.
+# At first we create a srfit Profile object that holds the observed data.
+
+from diffpy.srfit.fitbase import Profile
+linedata = Profile()
+linedata.setObservedProfile(xobs, yobs)
+# <demo> --- stop ---
+
+# The second step is to create FitContribution object, which associates
+# observed profile with a mathematical model for the dependent variable.
+
+from diffpy.srfit.fitbase import FitContribution
+linefit = FitContribution('linefit')
+linefit.setProfile(linedata)
+linefit.setEquation("A * x + B")
+# <demo> --- stop ---
+# <demo> auto
+
+# srfit objects can be examined by calling their show() function.
+# SrFit parses the model equation and finds two parameters A, B.
+# Their values are at this stage undefined.
+
 linefit.show()
-linefit.A=-0.5
-plot(xobs, linefit.evaluate())
+# <demo> --- stop ---
+
+# We can set A and B to some specific values and calculate model
+# observations.  The x and y attributes of the FitContribution are
+# the observed values, which can be resampled or truncated to a shorter
+# fitting range.
+
+linefit.A
+linefit.A = 3
+linefit.B = 5
+linefit.evaluate()
+
+# <demo> auto
+
+# We want to find optimum model parameters that fit the simulated curve
+# to the observations.  This is done by associating FitContribution with
+# a FitRecipe object.  FitRecipe can manage multiple fit contributions and
+# optimize all models to fit their respective profiles.
+
 from diffpy.srfit.fitbase import FitRecipe
-help(FitRecipe)
-rec=FitRecipe()
-help(rec.addContribution)
+rec = FitRecipe()
 rec.addContribution(linefit)
 rec.show()
-rec.linefit
-rec.linefit.A
-rec.linefit.A.value
-from scipy.optimize import fmin
-help(fmin)
-get_ipython().magic(u'pinfo rec.addVar')
+
+# <demo> --- stop ---
+
+# FitContributions may have many parameters.  We need to tell the recipe
+# object which of them should be tuned by the fit.
+
 rec.addVar(rec.linefit.A)
-rec.A
-rec.A.value
 rec.addVar(rec.linefit.B)
 rec.show()
-rec.names
-rec.values
-rec.residual()
-get_ipython().magic(u'pinfo rec.residual')
-rec.names
-rec.values
-rec.residual([1, 2])
-rec.show()
-rec.scalarResidual()
-get_ipython().magic(u'pinfo fmin')
-fmin(rec.scalarResidual, rec.values)
-clf()
-plot(xobs, yobs, '.')
-plot(xobs, linefit.evaluate())
+
+# <demo> auto
+
+# The addVar function created two attributes A, B for the rec object
+# which link ot the A and B parameters of the linefit contribution.
+
+print rec.A, rec.A.value
+
+# The names of the declared variables are stored in a rec.names
+# and the corresponding values in rec.values.
+
+print "rec.values =", rec.values
+print "rec.names =", rec.names
+
+# Finally the recipe objects provides a residual() function to calculate
+# the difference between the observed and simulated values.  The residual
+# function can accept a list of new variable values in the same order as
+# rec.names.
+
+print "rec.residual() =", rec.residual()
+print "rec.residual([2, 4]) =", rec.residual([2, 4])
+
+# <demo> --- stop ---
+
+# The FitRecipe.residual function can be directly used with the scipy
+# leastsq function for minimizing a sum of squares.
+
+from scipy.optimize import leastsq
+leastsq(rec.residual, rec.values)
+
+# <demo> --- stop ---
+
+# The FitRecipe.scalarResidual function returns the sum of squares and can
+# be used with a minimizer that expects scalar function:
+
+from scipy.optimize import fmin
+fmin(rec.scalarResidual, [1, 1])
+plot(linedata.x, linedata.ycalc)
+
+# <demo> --- stop ---
+
+# For a converged fit recipe, the details of the fit can be obtained
+# using the FitResults class.
+
 from diffpy.srfit.fitbase import FitResults
-get_ipython().magic(u'pinfo FitResults')
-help(FitResults)
+res = FitResults(rec)
+print res
+
+# <demo> --- stop ---
+
+# FIXME below
+DISABLE = '''
+# FitRecipe can be use to fix 
 res=FitResults(rec)
 print res
 rec.fix(B=0)
@@ -125,3 +168,4 @@ s=dumps(rec)
 from pickle import dumps
 s=dumps(rec)
 get_ipython().magic(u'save ex03.py 1-127')
+'''
