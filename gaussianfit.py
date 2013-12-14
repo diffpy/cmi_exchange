a8a86bf6cd7ee19a92aa338778061ac81f9bcdcf
Pavol Juhas <pavol.juhas@gmail.com>
2013-12-13 23:52:55 -0500

    Update module docstring.  Fix extension loading.

--- gaussianfit.py
+++ gaussianfit.py
@@ -1,40 +1,52 @@
 #!/usr/bin/env python
 ########################################################################
 #
-# diffpy.srreal     Complex Modeling Initiative
+# srfit-demos       Complex Modeling Initiative
 #                   Pavol Juhas
 #                   (c) 2013 Brookhaven National Laboratory,
 #                   Upton, New York.  All rights reserved.
-
-# File coded by:    Xiahao Yang
+#
+# File coded by:    Xiahao Yang, Kevin Knox
 #
 # See AUTHORS.txt for a list of people who contributed.
 # See LICENSE.txt for license information.
 #
 ########################################################################
-"""Example of ipython extension of fitting a Gaussian to simulated data.
 
-This is an example simply shows how to write an ipython extension for SrFit 
+"""IPython extension for SrFit fit of Gaussian peak to user data.
 
-Use:
+This is an example of defining a Gaussian peak fit using SrFit and
+its sharing as IPython extension.
+
+Usage in IPython shell:
+
+Install this extension with
+
     %install_ext https://raw.github.com/diffpy/srfit-demos/master/gaussianfit.py
-    
-Once to install, and then
+
+Activate this extension using
+
     %load_ext gaussianfit
-    
+
+or adjust the c.InteractiveShellApp.extensions variable in ipython_config.py
+to load it at startup every time.
+
+
 To try out the class execute the following commands:
-    x = linspace(-10,10,100)
+
+    import numpy as np
+    x = linspace(-10, 10, 100)
     A, x0, sig = 5, -2.5, 1.2
-    y = A * np.exp(-0.5*(x-x0)**2/sig**2) + np.random.normal(0,0.2,100)
-    newFit = GaussianFit(x, y)
-    newFit.refine()
-    newFit.plot()
-    
-    x, y, dy are gaussian date to be fitted (dy may be omitted)
-    A, sig, and x0 are initial values. If they are omitted the program
-    will estimate starting values.
-    
-This extension is based on srfit example gaussianrecipe.py 
+    y = A * np.exp(-0.5*(x-x0)**2/sig**2) + np.random.normal(0, 0.2, 100)
+    gfit = GaussianFit(x, y)
+    gfit.refine()
+    gfit.plot()
+
+    x, y, dy are the data to be fitted with Gaussian peak (dy may be omitted)
+    A, sigma, and x0 are initial values.  If omitted the program will estimate
+    their starting values.
+
+This extension is based on the SrFit example gaussianrecipe.py
 """
 
 from diffpy.srfit.fitbase import FitContribution, FitRecipe, Profile, FitResults
@@ -42,9 +54,9 @@ import numpy as np
 
 class GaussianFit(object):
     '''Data attributes:
-  
+
     Input data:
- 
+
     x    --  input x values (read only)
     y    --  input y values (read only)
     dy   --  input dy values (read only)
@@ -58,21 +70,21 @@ class GaussianFit(object):
 
 
     To try out the class execute the following commands:
-    x = linspace(-10,10,100)
+    x = linspace(-10, 10, 100)
     A, x0, sig = 5, -2.5, 1.2
-    y = A * np.exp(-0.5*(x-x0)**2/sig**2) + np.random.normal(0,0.2,100)
+    y = A * np.exp(-0.5*(x-x0)**2/sig**2) + np.random.normal(0, 0.2, 100)
     newFit = GaussianFit(x, y)
     newFit.refine()
     newFit.plot()
     '''
 
-    def __init__(self, x, y, dy = None, A = None, sig = None, x0 = None):
+    def __init__(self, x, y, dy=None, A=None, sig=None, x0=None):
         '''Create new GaussianFit object
-        ''' 
+        '''
         self._x = x
         self._y = y
         self._dy = np.ones_like(x) if dy is None else dy
-        
+
         if (A is None) or (sig is None) or (x0 is None):
             self._getStartingValues()
         else:
@@ -137,8 +149,8 @@ class GaussianFit(object):
         self._A = peakValue * self._sig
 
     def _makeRecipe(self):
-        '''Make a FitRecipe for fitting a Gaussian curve to data.    
-        ''' 
+        '''Make a FitRecipe for fitting a Gaussian curve to data.
+        '''
         profile = Profile()
         profile.setObservedProfile(self._x, self._y, self._dy)
 
@@ -151,7 +163,7 @@ class GaussianFit(object):
         recipe.addVar(contribution.A, self._A)
         recipe.addVar(contribution.x0, self._x0)
         recipe.addVar(contribution.sigma, self._sig)
-        
+
         self._recipe = recipe
         return
 
@@ -167,9 +179,9 @@ class GaussianFit(object):
         '''
         import pylab
         pylab.figure()
-        pylab.plot(self._x, self._y, 'b.', label = "observed Gaussian")
-        if self._yg is not None: pylab.plot(self._x, self._yg, 'g-', label = "calculated Gaussian")
-        pylab.legend(loc = (0.0,0.8))
+        pylab.plot(self._x, self._y, 'b.', label="observed Gaussian")
+        if self._yg is not None: pylab.plot(self._x, self._yg, 'g-', label="calculated Gaussian")
+        pylab.legend(loc=(0.0, 0.8))
         pylab.xlabel("x")
         pylab.ylabel("y")
         pylab.show()
@@ -177,7 +189,7 @@ class GaussianFit(object):
 
     def refine(self):
         '''Optimize the recipe created above using scipy.
-        ''' 
+        '''
         from scipy.optimize.minpack import leastsq
         print "Fit using scipy's LM optimizer"
         leastsq(self._recipe.residual, self._recipe.getValues())
@@ -187,6 +199,8 @@ class GaussianFit(object):
         self.printValues()
         return
 
+# end of class GaussianFit
+
 def load_ipython_extension(ip):
-    ip.user_ns['gaussianfit']=gaussianfit
+    ip.user_ns['GaussianFit'] = GaussianFit
     return
