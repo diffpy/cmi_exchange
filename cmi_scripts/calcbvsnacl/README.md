ea8795a39107420dbe869f9e5270931938d34686
Pavol Juhas <pavol.juhas@gmail.com>
2014-05-18 00:28:38 -0400

    Demo the Bond Valence Sum calculator.
    Example calculation for NaCl and optional customization of
    bond valence parameters.

---
+++ cmi_scripts/calcbvsnacl/README.md
@@ -0,0 +1,7 @@
+# Calculate bond valence sums for NaCl
+
+This example shows how to evaluate bond valence sums using the `diffpy.srreal`
+module in DiffPy-CMI.  BVS calculator uses a standard set of bond valence
+parameters obtained from a lookup table.  Bond valence parameters can be
+set to custom values.  Multiple BVS calculator objects can be created each
+one with a separate lookup table and optionally different valence parameters.
---
+++ cmi_scripts/calcbvsnacl/calcbvs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Calculation of bond valence sums using diffpy.srreal module included in
+DiffPy-CMI.
+'''
+
+from diffpy.Structure import loadStructure
+from diffpy.srreal.bvscalculator import BVSCalculator
+
+# load crystal structure data from a CIF file
+nacl = loadStructure('NaCl.cif')
+
+# create bond valence sum calculator object
+bvsc = BVSCalculator()
+
+# calculate BVS and print the expected and actual valences
+vsim = bvsc(nacl)
+print 'Calculate bond valence sums for NaCl "bvsc(nacl)"'
+print 'expected "bvsc.valences":\n ', bvsc.valences
+print 'calculated "bvsc.value":\n ' , vsim
+print 'difference "bvsc.bvdiff":\n ', bvsc.bvdiff
+print 'root mean square difference "bvsc.bvrmsdiff":', bvsc.bvrmsdiff
+print
+
+# create new BVS calculator with a custom bond valence parameters
+bvsc2 = BVSCalculator()
+
+# Use alternate value for the Na+ Cl- bond valence parameters from
+# http://www.iucr.org/__data/assets/file/0018/59004/bvparm2011.cif
+# These parameters have 6A cutoff.
+bvsc2.bvparamtable.setCustom('Na', +1, 'Cl', -1, Ro=1.6833, B=0.608)
+bvsc2.rmax = 6
+print "BVS in NaCl with alternate parameters:\n ", bvsc2(nacl)
+print
+
+# Lookup table of bond valence parameters can be used as separate object.
+from diffpy.srreal.bvparameterstable import BVParametersTable
+
+table = BVParametersTable()
+bp = table.lookup('Na+', 'Cl-')
+bp2 = table.lookup('Na', +1, 'Br', -1)
+print "Standard lookup of bond valence parameters:"
+print " ", bp
+print " ", bp2
+print
+
+print "Handling of unknown or invalid ion pairs:"
+print "  table.lookup('A+', 'X-'):", table.lookup('A+', 'X-')
+print "  table.lookup('A+', 'X-') == table.none():", (
+        table.lookup('A+', 'X-') == table.none())
